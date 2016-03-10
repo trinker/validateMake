e32f063fd5fe6ebccbbde3f561a1577c7b6752e8
Tyler Rinker <trinker@campuslabs.com>
2016-03-10 09:29:07 -0500

    added comment to script

--- inst/extradocs/validate.R
+++ inst/extradocs/validate.R
@@ -14,7 +14,7 @@ valiData_available <- require('valiData')
 
 if (!valiData_available) {
 	cat(
-         sprintf(html_message , "`valiData` not installed."), 
+         sprintf(html_message , "`valiData` not installed."),
 	     file = file.path(Sys.getenv("USERPROFILE"),'Desktop/ERROR.html')
 	)
 	browseURL(file.path(Sys.getenv("USERPROFILE"),'Desktop/ERROR.html'))
@@ -27,32 +27,32 @@ path <- list.dirs(recursive = FALSE)
 
 if (length(path) > 1) {
 	cat(
-         sprintf(html_message , "More than one directory located in 'TestCore'.<br>Remove additional file.<br><br>If that doesn't work...<br>"), 
+         sprintf(html_message , "More than one directory located in 'TestCore'.<br>Remove additional file.<br><br>If that doesn't work...<br>"),
 	     file = file.path(Sys.getenv("USERPROFILE"),'Desktop/ERROR.html')
 	)
 	browseURL(file.path(Sys.getenv("USERPROFILE"),'Desktop/ERROR.html'))
-	stop("Error occurred")	
+	stop("Error occurred")
 }
 
 ## Check to make sure a folder is in TestCore
 if (length(path) == 0) {
 	cat(
-         sprintf(html_message , "It appears there is no test directory located in 'TestCore'.<br>Put a file in TestCore.<br><br>If that doesn't work...<br>"), 
+         sprintf(html_message , "It appears there is no test directory located in 'TestCore'.<br>Put a file in TestCore.<br><br>If that doesn't work...<br>"),
 	     file = file.path(Sys.getenv("USERPROFILE"),'Desktop/ERROR.html')
 	)
 	browseURL(file.path(Sys.getenv("USERPROFILE"),'Desktop/ERROR.html'))
-	stop("Error occurred")	
+	stop("Error occurred")
 }
 
 ## Check if the map is available and read in
 map_loc <- "L:/swiper/DataScience/data_quality/core_data_mapping/core_data_map.rds"
 if (!file.exists(map_loc)) {
 	cat(
-         sprintf(html_message , "The Data Map appears to be missing."), 
+         sprintf(html_message , "The Data Map appears to be missing."),
 	     file = file.path(Sys.getenv("USERPROFILE"),'Desktop/ERROR.html')
 	)
 	browseURL(file.path(Sys.getenv("USERPROFILE"),'Desktop/ERROR.html'))
-	stop("Error occurred")	
+	stop("Error occurred")
 }
 
 
@@ -63,11 +63,11 @@ core_data_map <- readRDS(map_loc)
 col_map_loc <- "L:/swiper/DataScience/data_quality/core_data_mapping/column_mapping.rds"
 if (!file.exists(col_map_loc)) {
 	cat(
-         sprintf(html_message , "The Column Tests Map appears to be missing."), 
+         sprintf(html_message , "The Column Tests Map appears to be missing."),
 	     file = file.path(Sys.getenv("USERPROFILE"),'Desktop/ERROR.html')
 	)
 	browseURL(file.path(Sys.getenv("USERPROFILE"),'Desktop/ERROR.html'))
-	stop("Error occurred")	
+	stop("Error occurred")
 }
 
 
@@ -82,11 +82,11 @@ did_it_work <- try(valiData::valiData(path, core_data_map, col_map))
 ## Otherwise give error in browser
 if (inherits(did_it_work, "try-error")) {
 	cat(
-        sprintf(html_message , "Some sort of error occurred in `valiData` function."), 
+        sprintf(html_message , "Some sort of error occurred in `valiData` function."),
 	    file = file.path(Sys.getenv("USERPROFILE"),'Desktop/ERROR.html')
 	)
 	browseURL(file.path(Sys.getenv("USERPROFILE"),'Desktop/ERROR.html'))
-	stop("Error occurred")	
+	stop("Error occurred")
 } else {
 
 	## make VALIDATED_DATA folder on Desktop and move over folder from TestCore
@@ -99,14 +99,15 @@ if (inherits(did_it_work, "try-error")) {
         unlink(path, recursive = TRUE, force = FALSE)
     } else {
     	cat(
-            sprintf(html_message , "Folder not moved to Desktop.<br>Please manually move folder out of TestCore.<br><br>If this problem persists..<br>"), 
+            sprintf(html_message , "Folder not moved to Desktop.<br>Please manually move folder out of TestCore.<br><br>If this problem persists..<br>"),
     	    file = file.path(Sys.getenv("USERPROFILE"),'Desktop/ERROR.html')
     	)
     	browseURL(file.path(Sys.getenv("USERPROFILE"),'Desktop/ERROR.html'))
-	    stop("Error occurred")    	
+	    stop("Error occurred")
     }
 }
 
+## send user name and time stamp for user research
 if (check_gf){
 
     form <- "https://docs.google.com/forms/d/1t4g3F2f1bXUO5Xr00iRR6Kah07WAJK3WSJvm5ja7kOE/viewform"
